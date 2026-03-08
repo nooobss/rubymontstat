3b6866bcc55938bb5ce2182f3a750d9cfb2b74a4
uciharis <uciharis@hotmail.com>
2026-03-08 22:23:25 +0700

    ngerjain exercise number

--- 001.rb
+++ 001.rb
@@ -25,3 +25,20 @@ integer_div = 3 / 2
 puts integer_div
 float_div = 3 / 2.0
 puts float_div
+
+## Exercise Number
+## exercise 1.1
+puts "hours in year : #{24*365} hours"
+puts "minutes in decade : #{60*24*365*10} minutes"
+puts "seconds in age : #{100*365*24*3600} seconds"
+
+## exercise 1.2
+puts 3.0 / 2
+puts 3 / 2.0
+puts 4 ** 2.0
+puts 4.1 % 2
+
+## exercise 1.3
+puts 8374.even?
+
+## exercise 1.4
