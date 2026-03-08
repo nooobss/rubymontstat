f2e1998403c2431ea4e01602ea3c5c2ccfbb0641
uciharis <uciharis@hotmail.com>
2026-03-08 22:03:37 +0700

    variabel dan numbers behaviour

--- 001.rb
+++ 001.rb
@@ -1,6 +1,27 @@
-# eksekusi kode di terminal bukan eksekusi filenya
+# eksekusi kode di terminal, bukan eksekusi filenya
 # biasanya eksekusi kode di terminal menggunakan irb
 =begin
 ruby -e "puts 123"
 
 =end
+# Variabel
+number = 4
+puts number
+number = number * 99
+puts number
+puts number + 4
+
+# Number
+jemboet = 3.14
+puts jemboet
+
+integer_num = 23 + 17
+puts integer_num
+float_num = 23.0 + 17.0
+puts float_num
+
+## berlaku juga untuk pembagian
+integer_div = 3 / 2
+puts integer_div
+float_div = 3 / 2.0
+puts float_div
